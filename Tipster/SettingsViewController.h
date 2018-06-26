cbc68c2ceeca2aaf9d57d509b2cf84d0e92983f7
Rucha Patki <ruchapatki@fb.com>
2018-06-26 15:46:25 -0700

    Added gitignore

---
+++ Tipster/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Tipster
+//
+//  Created by Rucha Patki on 6/26/18.
+//  Copyright © 2018 Rucha Patki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
